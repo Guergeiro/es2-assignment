33606ac48015d43ce8d6ef4ea25fe15b4bda7232
Guergeiro <business@brenosalles.com>
2020-05-08 14:52:14 +0100

    Added Exceptions

---
+++ src/main/java/com/brenosalles/reqres/exceptions/BadRequestException.java
@@ -0,0 +1,9 @@
+package com.brenosalles.reqres.exceptions;
+
+public class BadRequestException extends Exception {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/java/com/brenosalles/reqres/exceptions/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.brenosalles.reqres.exceptions;
+
+public class NotFoundException extends Exception {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+}
