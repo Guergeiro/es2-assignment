c546256bb7006fce759a7a9695cbebdef5ace82c
Guergeiro <business@brenosalles.com>
2020-05-17 16:36:50 +0100

    Added tokens cache

---
+++ src/main/java/com/brenosalles/reqres/cache/exceptions/TokenNotFound.java
@@ -0,0 +1,10 @@
+package com.brenosalles.reqres.cache.exceptions;
+
+public class TokenNotFound extends Exception {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+}
